6883799083c4489aaf1ffe6a8e3702f51c5f5e9b
Mihai Minea <mineamihai2001@gmail.com>
2021-10-21 17:49:06 +0300

    first commit

---
+++ client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+
+#define FIFO_NAME "Comanda"
+
+int main(int argc, char* argv[])
+{
+    
+}
