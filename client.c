d2ccaba472ed8513d09d92c210d56d0c85933ce5
Mihai Minea <mineamihai2001@gmail.com>
2021-10-23 11:55:17 +0300

    fifo

--- client.c
+++ client.c
@@ -4,10 +4,32 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <string.h>
+#include <fcntl.h>
 
 #define FIFO_NAME "Comanda"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    
+
+    char comanda[20];
+    int num, fd;
+
+    printf("Asteptare server ...\n");
+    fd = open(FIFO_NAME, O_WRONLY);
+    printf("Server conectat, introduceti o comanda ...\n");
+
+    while (fgets(comanda, sizeof(comanda), stdin), !feof(stdin))
+    {
+        if ((num = write(fd, comanda, strlen(comanda))) == -1)
+        {
+            perror("Eroare la scriere in pipe\n");
+            exit(0);
+        }
+        else
+        {
+            printf("S-a scris comanda %s\n", comanda);
+        }
+    }
 }
+
--- server.c
+++ server.c
@@ -4,10 +4,75 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
+#include <fcntl.h>
+#include <string.h>
 
 #define FIFO_NAME "Comanda"
 
-int main(int argc, char* argv[])
-{
+int este_comanda(char comanda[], char username[]);
 
+int main(int argc, char *argv[])
+{
+    int num, fd;
+    char comanda[20], username[20];
+
+    mknod(FIFO_NAME, S_IFIFO | 0666, 0);
+
+    printf("Asteptare client ...\n");
+    fd = open(FIFO_NAME, O_RDONLY);
+    printf("Client conectat\n");
+
+    do
+    {
+        if ((num = read(fd, comanda, 300)) == -1)
+            perror("Eroare la citire din pipe");
+        else
+        {
+            //comanda[num] = '\0';
+            printf("S-a citit din pipe comanda %s\n", comanda);
+            este_comanda(comanda, username);
+        }
+    } while (num > 0);
+}
+
+int este_comanda(char comanda[], char username[])
+{
+    if (strncmp(comanda, "login : ", 7) == 0)
+    {
+        int j = 0;
+        for (int i = 8; i <= strlen(comanda) - 1; i++)
+        {
+            username[j] = comanda[i];
+            ++j;
+        }
+
+        printf("S-a dat comanda login cu username: %s\n", username);
+        return 1;
+    }
+    else if (strncmp(comanda, "get-logged-users", 15) == 0)
+    {
+
+        printf("S-a dat comanda %s", comanda);
+        return 1;
+    }
+    else if (strncmp(comanda, "get-proc-info", 12) == 0)
+    {
+        printf("S-a dat comanda %s\n", comanda);
+        return 1;
+    }
+    else if (strncmp(comanda, "logout", 5) == 0)
+    {
+        printf("S-a dat comanda %s\n", comanda);
+        return 1;
+    }
+    else if (strncmp(comanda, "exit", 3) == 0)
+    {
+        printf("S-a dat comanda %s\n", comanda);
+        return 1;
+    }
+    else
+    {
+        printf("Eroare: comanda gresita! Introduceti o comanda corecta!\n%s\n", comanda);
+        return 0;
+    }
 }
